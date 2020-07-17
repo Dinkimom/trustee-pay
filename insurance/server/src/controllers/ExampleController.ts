0fe4eb850645aa4bfcdd0f6cc792b6dcb56e922a
Nikita Dmitriev <nikitadmitriev97@mail.ru>
2020-07-17 23:03:49 +0400

    Base server structure

---
+++ insurance/server/src/controllers/ExampleController.ts
@@ -0,0 +1,48 @@
+/**
+ * Example controller
+ *
+ * created by Sean Maxwell Apr 14, 2019
+ */
+
+import { Controller, Delete, Get, Post, Put } from '@overnightjs/core';
+import { Logger } from '@overnightjs/logger';
+import { Request, Response } from 'express';
+
+@Controller('api')
+export class ExampleController {
+    @Get(':msg')
+    private getMessage(req: Request, res: Response) {
+        Logger.Info(req.params.msg);
+        res.status(200).json({
+            message: req.params.msg,
+        });
+    }
+
+    @Put(':msg')
+    private putMessage(req: Request, res: Response) {
+        Logger.Info(req.params.msg);
+        return res.status(400).json({
+            error: req.params.msg,
+        });
+    }
+
+    @Post(':msg')
+    private postMessage(req: Request, res: Response) {
+        Logger.Info(req.params.msg);
+        return res.status(400).json({
+            error: req.params.msg,
+        });
+    }
+
+    @Delete(':msg')
+    private delMessage(req: Request, res: Response) {
+        try {
+            throw new Error(req.params.msg);
+        } catch (err) {
+            Logger.Err(err, true);
+            return res.status(400).json({
+                error: req.params.msg,
+            });
+        }
+    }
+}
